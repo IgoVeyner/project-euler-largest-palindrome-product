c095d5838693d10bb99481ea7b6ff31852f22c01
Igor Veyner <igorveyner95@gmail.com>
2021-05-19 06:39:48 -0400

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,9 +1,20 @@
 # Implement your object-oriented solution here!
 class LargestPalindromeProduct
-  attr_accessor :answer
 
   def initialize
   end
 
+  def answer 
+    max = 0
+
+    (0...999).each do |i|
+        (0...999).each do |j|
+          product = i * j
+          max = product if is_palindrome?(product) && product > max
+        end
+    end
+    
+    max 
+  end
 
 end
